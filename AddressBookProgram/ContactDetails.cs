83fc17ad0777e8e4c6b942c67ca087ffd57767cc
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-04 17:17:03 +0530

    Refactor:AddressBook

---
+++ AddressBookProgram/ContactDetails.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProgram
+{
+    /// <summary>
+    //Addressbookprogram using oops concept
+    //by using interface and  constructos
+    /// </summary>
+    interface IContacts
+    {
+        void Addressbook();
+    }
+    class ContactDetails : IContacts
+    {      
+        //variables
+        public string firstname;
+         public string lastname;
+         public  string address;
+         public string city;
+         public string state;
+         public string email;
+         public int zipcode;
+         public long phonenumber;
+       
+        //using constructors
+        public ContactDetails(string firstname, string lastname, string address, string city,
+                                  string state, string email, int zipcode, long phonenumber)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.email = email;
+            this.zipcode = zipcode;
+            this.phonenumber = phonenumber;
+        }
+
+        public void Addressbook()
+        {            
+            Console.WriteLine("Enter the details:");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+    
+
--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -7,6 +7,17 @@ namespace AddressBookProgram
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to AddressBookSystem");
+            ContactDetails contact = new ContactDetails
+              ( "Sree","Ramu", "NehruColony","Hyderabad","Telangana",
+                       "akhila@gmail.com",509132,67834215678);
+            contact.Addressbook();
+            
+            Console.WriteLine(contact.firstname+ "" + contact.lastname);
+            Console.WriteLine (contact.address +"" +contact.state );
+            Console.WriteLine(contact.city + "");
+            Console.WriteLine(contact.email + "");
+            Console.WriteLine(contact.zipcode + "");
+            Console.WriteLine (""+contact.phonenumber);
         }
     }
 }
